92b375698bbb5e1c91a8d2fc7f53e88549bfb0d1
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-17 19:00:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 =62 , num2 =1
+const multiply =(num1 *num2)
+
+const random=Math.floor(Math.random()*10)
+
+const num3=12, num4= 3;
+const mod=(num3 / num4);
+
+const max=Math.max (15,16,17,18,19,20);
